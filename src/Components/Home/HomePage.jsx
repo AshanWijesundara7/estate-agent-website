8c7ca49fd431b8a75e22aa37e95441d0f1a3f2b0
AshanMalidu <ashan.20230392@iit.ac.lk>
2026-01-05 05:30:42 +0530

    error fixing

--- src/Components/Home/HomePage.jsx
+++ src/Components/Home/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./Homepage.css"; 
+import "./HomePage.css"; 
 
 const Homepage = () => {
   const navigate = useNavigate();
